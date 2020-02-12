1b14d0d6a2d463db2648ddf376c0bf80cc84f23a
Supardi <supardi9512@gmail.com>
2020-02-12 22:38:57 +0700

    mengkonversi halaman detail travel dan membuat controller detail

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -9,9 +9,9 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>SB Admin 2 - Dashboard</title>
+  <title>NOMADS Admin</title>
 
-  @include('includes.style')
+  @include('includes.admin.style')
 
 </head>
 
@@ -20,19 +20,19 @@
   <!-- Page Wrapper -->
   <div id="wrapper">
 
-    @include('includes/sidebar')
+    @include('includes.admin.sidebar')
 
     <!-- Content Wrapper -->
     <div id="content-wrapper" class="d-flex flex-column">
 
       <!-- Main Content -->
       <div id="content">
-        @include('includes/navbar')
+        @include('includes.admin.navbar')
         @yield('content')
       </div>
       <!-- End of Main Content -->
 
-      @include('includes/footer')
+      @include('includes.admin.footer')
     </div>
     <!-- End of Content Wrapper -->
 
@@ -63,7 +63,7 @@
     </div>
   </div>
 
-  @include('includes/script')
+  @include('includes.admin.script')
 
 </body>
 
