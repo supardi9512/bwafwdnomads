384546109b12119245bf329f73f644ab99f51773
Supardi <supardi9512@gmail.com>
2020-02-22 23:00:35 +0700

    membuat model gallery

---
+++ app/Gallery.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Gallery extends Model
+{
+    use SoftDeletes; // mengaktifkan soft deletes
+
+    // menyimpan secara langsung
+    protected $fillable = [
+        'travel_packages_id', 'image'
+    ];
+
+    protected $hidden = [
+
+    ];
+
+    public function travel_package() {
+        return $this->belongsTo(TravelPackage::class, 'travel_packages_id', 'id');
+    }
+}
